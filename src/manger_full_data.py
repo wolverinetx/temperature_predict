68e369d2c2aa6d8a80406a9aa927c69e8022a15f
Wolverinetx <wolverinetx@hotmail.com>
2018-06-05 23:33:55 -0500

    uso de pandas

--- src/manger_full_data.py
+++ src/manger_full_data.py
@@ -1,14 +1,21 @@
 
-from pandas import pandas
+import pandas as pd
 import os
-
+import numpy as np
+import datetime
 
 def main():
     print('hola')
     dirname=os.path.dirname(__file__)
-    filename=os.path.join(dirname, '../data/data.csv')
-    df=pandas.read_csv(filename, sep=';',header=None, na_values="NaN")
-    print(df)
+    filename=os.path.join(dirname, '../data/temperatura.csv')
+    df=pd.read_csv(filename, sep=',',header=0, na_values="NaN")
+
+    temperaturadf=pd.DataFrame(df)
+    print(temperaturadf.info())
+    print(temperaturadf.describe())
+
+    df2 = df.copy()
+
 
 if __name__=='__main__':
             main()
